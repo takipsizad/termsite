e12b95b3932c01acbf317cb419166231995a1e3b
takipsizad <addeniz25@gmail.com>
2021-01-09 12:43:02 +0300

    hey

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',{title:'termsite'});
+router.get('/', function(req, res) {
+  res.sendFile( path.resolve('./views/index.html') );
 });
 module.exports = router;
