160b83a680f482d375440138fa7af1c2875ce315
takipsizad <addeniz25@gmail.com>
2021-01-11 22:45:49 +0300

    eeeeeeee

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');-
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -39,6 +39,7 @@ app.use('/api/randommessage', randommessage);
 app.use('/serverinfo', serverinfo);
 app.use('/funding', funding);
 app.use('/api/dblwebhook', dblwebhook);
+app.use('/favicon.ico', favicon)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
