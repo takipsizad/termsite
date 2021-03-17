388e5c608aeb60ddc0f34b544203c30ab94b0bdb
takipsizad <addeniz25@gmail.com>
2021-03-17 14:18:16 +0300

    lol

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(limiter);
 app.use(speedLimiter);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 
 app.use(logger('dev'));
